767d492043bed966f5326ada0d3df22b17074646
thariqi01 <91468867+thariqi01@users.noreply.github.com>
2021-10-24 21:55:35 +0700

    commit9.1

--- resources/views/Form tambah/roleadd.blade.php
+++ resources/views/Form tambah/roleadd.blade.php
@@ -32,10 +32,10 @@
         <div class="card">
             <div class="card-header">
                 <div class="pull-left">
-                    <strong>Tambah Role</strong>
+                    <strong>Edit Role</strong>
                 </div>
                 <div class="pull-right">
-                    <a href="{{ url('user') }}" class="btn btn-secondary btn-sm">
+                    <a href="{{ url('role') }}" class="btn btn-secondary btn-sm">
                         <i class="fa fa-undo"></i> Back
                     </a>
                 </div>
@@ -44,21 +44,21 @@
                 
                 <div class="row">
                     <div class="col-md-4 offset-md-4">
-                                <form action="{{ url('user/store') }}" method="post">
+                                <form action="{{ url('role/store') }}" method="post">
                                     @csrf
                                 <div class="form-group">
-                                    <label for="nama_user">Nama Role</label>
+                                    <label for="nama_role">Nama Role</label>
                                         <div class="input-group">
                                             <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-                        <input type="text"  class="form-control" name="nama_user" placeholder="Masukkan nama"/>
+                                            <input type="text"  class="form-control" name="nama_role" placeholder="Masukkan nama"/>
                                     </div>
                                 </div>
         
                                 <div class="form-group">
-                                    <label for="email">Edit</label>
+                                    <label for="edit">Edit</label>
                                         <div class="input-group">
                                             <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-                                            <input type="text" class="form-control" name="email" placeholder="Masukkan Email"/>
+                                            <input type="text" class="form-control" name="edit" placeholder="Masukkan Email"/>
                                     </div>
                                 </div>
                                         
